2198dd0431081b3f7094eabf8e2c74f853622e54
Pratap Kotala <pk@yahoo.com>
2025-03-04 15:29:29 -0600

    Adding Starter files for the project

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { ChildComponent } from './child/child.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, ChildComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'practice-Component-Interaction';
+}
