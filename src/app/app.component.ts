42e926a27d9fff10197ca33f9e0cdba67639962f
zqnd3r <brickinthewall9@gmail.com>
2025-03-17 11:51:43 -0500

    changes CAO 17th Mar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ChildComponent } from './child/child.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ChildComponent],
+  imports: [RouterOutlet, ChildComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'practice-Component-Interaction';
+  pText:string = '';
 }
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -8,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class ChildComponent {
 
+    @Input() inputText: string = '';
+
+    @Output() outputValue: EventEmitter<string> = new EventEmitter<string>();
 }
